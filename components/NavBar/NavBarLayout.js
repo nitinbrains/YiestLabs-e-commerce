91f16929fccfeb7ab9cb0db172ca75a9b8e7b45e
Carlos Urdampilleta <curd@ucdavis.edu>
2018-09-06 00:34:24 +0200

    Hover User Bar Width Fix. Getting ready for hoverable items

--- components/NavBar/NavBarLayout.js
+++ components/NavBar/NavBarLayout.js
@@ -6,69 +6,73 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 
 class NavBarLayout extends Component {
-  render() {
-    const { children, classes, theme } = this.props;
+    render() {
+        const { children, classes, theme } = this.props;
 
-    return (
-      <div className={classes.root}>
-        <AppBar className={classes.appBar} position="absolute">
-          <Toolbar>
-            <div className={classes.logo}>
-              <img src="../../static/images/logoHeader.png" height="40" />
+        return (
+            <div className={classes.root}>
+                <AppBar className={classes.appBar} position="absolute">
+                    <Toolbar>
+                        <div className={classes.logo}>
+                            <img
+                                src="../../static/images/logoHeader.png"
+                                height="40"
+                            />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    {children}
+                </main>
             </div>
-          </Toolbar>
-        </AppBar>
-
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          {children}
-        </main>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-    zIndex: 1,
-    overflow: "hidden",
-    position: "relative",
-    display: "flex",
-    width: "100%"
-  },
-  logo: {
-    flexGrow: 1
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    padding: "0 8px",
-    ...theme.mixins.toolbar
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  }
+    root: {
+        zIndex: 1,
+        overflow: "hidden",
+        position: "relative",
+        display: "flex",
+        width: "100%"
+    },
+    logo: {
+        flexGrow: 1
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    toolbar: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: "0 8px",
+        ...theme.mixins.toolbar
+    },
+    content: {
+        flexGrow: 1,
+        marginTop: 10,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    }
 });
 
 NavBarLayout.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired
 };
 
 export default withStyles(styles, { withTheme: true })(NavBarLayout);
