0f8a5a086842c83f281920cfccb5e910b948c7cb
Dimitar Vasilev <dimitrovasilev@gmail.com>
2018-11-28 18:08:21 -0800

    filtering inventory works again

--- redux/reducers/inventoryReducer.js
+++ redux/reducers/inventoryReducer.js
@@ -5,69 +5,6 @@ const initialState = {
     items: [],
     itemsToShow: [],
     error: null,
-    categories: [{
-        label: "Yeast",
-        value: 0,
-        checked: true,
-        subCategories: [{
-            label: "Ale Strains",
-            value: 1,
-            checked: false
-        }, {
-            label: "Lager Strains",
-            value: 2,
-            checked: false
-        }, {
-            label: "Wine/Mead/Cider Strains",
-            value: 3,
-            checked: false
-        }, {
-            label: "Distilling Strains",
-            value: 4,
-            checked: false
-        }, {
-            label: "Specialty/Belgian Strains",
-            value: 5,
-            checked: false
-        }, {
-            label: "Wild Yeast & Bacteria Strains",
-            value: 6,
-            checked: false
-        }, {
-            label: "Vault Strains",
-            value: 7,
-            checked: false
-        }]
-    }, {
-        label: "Enzymes & Nutrients",
-        value: 8,
-        checked: false,
-        subCategories: [{
-            label: "Enzymes",
-            value: 9,
-            checked: false
-        }, {
-            label: "Nutrients",
-            value: 10,
-            checked: false
-        }]
-    }, {
-        label: "Analytical Lab Services",
-        value: 12,
-        checked: false
-    }, {
-        label: "Lab Supplies",
-        value: 13,
-        checked: false
-    }, {
-        label: "Education",
-        value: 14,
-        checked: false
-    }, {
-        label: "Gift Shop",
-        value: 15,
-        checked: false
-    }],
     isLoading: false
 };
 
@@ -76,12 +13,16 @@ export default createReducer(initialState, {
     [inventoryTypes.GET_INVENTORY_ATTEMPT]: (state, { data }) => ({
         isLoading: true,
     }),
-    [inventoryTypes.GET_INVENTORY_SUCCESS]: (state, { data: { itemsToShow } }) => ({
+    [inventoryTypes.GET_INVENTORY_SUCCESS]: (state, { data: { itemsToShow, items } }) => ({
         itemsToShow,
+        items,
         isLoading: false
     }),
     [inventoryTypes.GET_INVENTORY_FAILURE]: (state, { data }) => ({
         isLoading: false,
+    }),
+    [inventoryTypes.CHANGE_CATEGORY_SUCCESS]: (state, { data: { itemsToShow } }) => ({
+        itemsToShow,
     }),
     [inventoryTypes.SEARCH_STRAIN_SUCCESS]: (state, { data: { itemsToShow } }) => ({
         itemsToShow,
