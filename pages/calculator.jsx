ce6828a01dde995a157fb1bda42a5202b3881479
Carlos Urdampilleta <curd@ucdavis.edu>
2018-12-18 00:51:37 +0100

    Calculator navbar fix

--- pages/calculator.jsx
+++ pages/calculator.jsx
@@ -1,80 +1,79 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
-import NavBarLayout from "../components/NavBar/NavBarLayout";
-import Grid from '@material-ui/core/Grid';
+import NavBarUserSearchDrawerLayout from "../components/NavBar/NavBarUserSearchDrawerLayout";
+import Grid from "@material-ui/core/Grid";
 import Dialog from "@material-ui/core/Dialog";
 
 // custom
-import CalculatorForm from '../components/Calculator/CalculatorForm';
-import CalculatorResult from '../components/Calculator/CalculatorResult';
+import CalculatorForm from "../components/Calculator/CalculatorForm";
+import CalculatorResult from "../components/Calculator/CalculatorResult";
 
-import Alert from '../components/UI/Alert';
-import FormTextbox from '../components/Form/FormTextbox'
-import FormSelectbox from '../components/Form/FormSelectbox'
-import FormButton from '../components/Form/FormButton'
-import FormCheckbox from '../components/Form/FormCheckbox'
+import Alert from "../components/UI/Alert";
+import FormTextbox from "../components/Form/FormTextbox";
+import FormSelectbox from "../components/Form/FormSelectbox";
+import FormButton from "../components/Form/FormButton";
+import FormCheckbox from "../components/Form/FormCheckbox";
 
-import { calculatorActions } from '../redux/actions/calculatorActions'
+import { calculatorActions } from "../redux/actions/calculatorActions";
 
 class Calculator extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			showResultDialog: true,
-			result: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            showResultDialog: true,
+            result: {}
+        };
     }
-	}
 
-	static getDerivedStateFromProps(nextProps, prevState){
-	 	return {
-			showResultDialog: nextProps.calculator.showResult,
-			result: nextProps.calculator.result
-	  }
-	  // return null
-	}
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+            showResultDialog: nextProps.calculator.showResult,
+            result: nextProps.calculator.result
+        };
+        // return null
+    }
 
-	render() {
-		return (
-			<NavBarLayout>
-				<div id="calculator-box">
-					<CalculatorForm
-						onSubmit={this.props.startCalculate}
-					/>
+    render() {
+        return (
+            <NavBarUserSearchDrawerLayout>
+                <div id="calculator-box">
+                    <CalculatorForm onSubmit={this.props.startCalculate} />
 
-					<Dialog
-              open={this.state.showResultDialog}
-              onClose={()=> { this.props.closeDialog()}}
-              aria-labelledby="form-dialog-title"
-          >
-          <CalculatorResult
-          	summary={this.state.result.summary}
-          	items={this.state.result.items}
-          />
-          </Dialog>
-
-
-				</div>
-			</NavBarLayout>
-		);
-	}
+                    <Dialog
+                        open={this.state.showResultDialog}
+                        onClose={() => {
+                            this.props.closeDialog();
+                        }}
+                        aria-labelledby="form-dialog-title"
+                    >
+                        <CalculatorResult
+                            summary={this.state.result.summary}
+                            items={this.state.result.items}
+                        />
+                    </Dialog>
+                </div>
+            </NavBarUserSearchDrawerLayout>
+        );
+    }
 }
 
-const styles = theme => ({
-});
+const styles = theme => ({});
 
-Calculator.propTypes = {
+Calculator.propTypes = {};
+
+const mapStateToProps = state => {
+    return {
+        calculator: state.calculator
+    };
 };
 
-const mapStateToProps = (state) => {
-  return {
-  	calculator: state.calculator
-	}
-}
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(calculatorActions, dispatch);
 
-const mapDispatchToProps = dispatch => bindActionCreators(calculatorActions, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(Calculator));
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles, { withTheme: true })(Calculator));
