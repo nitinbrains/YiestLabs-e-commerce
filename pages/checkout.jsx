47e20a5c8e0d81ee45051ee3a3d902332d8eaa6a
Carlos Urdampilleta <curd@ucdavis.edu>
2018-12-18 01:37:51 +0100

    Changed checkout box

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { orderActions } from '../redux/actions/orderActions';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { orderActions } from "../redux/actions/orderActions";
 
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -20,172 +20,191 @@ import Items from "../components/Checkout/Items/Items";
 import Review from "../components/Checkout/Review/Review";
 
 // custom
-import Alert from '../components/UI/Alert';
+import Alert from "../components/UI/Alert";
 
 const steps = ["Shipping", "Billing", "Items", "Review your order"];
 
 function getStepContent(step) {
-	switch (step) {
-		case 0:
-			return <Shipping />;
-		case 1:
-			return <Billing />;
-		case 2:
-			return <Items />;
-		case 3:
-			return <Review />;
-		default:
-			throw new Error("Unknown step");
-	}
-}
-
-class Checkout extends Component {
-
-	constructor(props) {
-		super(props);
-	}
-
-	componentWillMount() {
-		this.props.prepareOrder();
-	}
-
-	state = {
-		activeStep: 0
-	};
-
-	handleNext = () => {
-		const { activeStep } = this.state;
-		this.setState({
-			activeStep: activeStep + 1
-		});
-	};
-
-	handleBack = () => {
-		const { activeStep } = this.state;
-		this.setState({
-			activeStep: activeStep - 1
-		});
-	};
-
-	handleReset = () => {
-		this.setState({
-			activeStep: 0
-		});
-	};
-
-	render() {
-		const { classes } = this.props;
-		const { activeStep } = this.state;
-
-		return (
-			<NavBarLayout>
-
-				{this.props.message.messages.map((message, i) => <Alert message={message} index={i}/> )}
-
-				<div className={classes.layout}>
-					<Card>
-						<CardHeader color="primary">
-							<Typography color="secondary" variant="display1" align="center">
-								Checkout
-							</Typography>
-						</CardHeader>
-
-						<CardBody>
-							<Stepper activeStep={activeStep} className={classes.stepper}>
-								{steps.map(label => (
-									<Step key={label}>
-										<StepLabel>{label}</StepLabel>
-									</Step>
-								))}
-							</Stepper>
-							<React.Fragment>
-								{activeStep === steps.length ? (
-									<React.Fragment>
-										<Typography variant="headline" gutterBottom>
-											Thank you for your order.
-										</Typography>
-										<Typography variant="subheading">
-											Your order number is #2001539. We have emailed your oder
-											confirmation, and will send you an update when your order
-											has shipped.
-										</Typography>
-									</React.Fragment>
-								) : (
-									<React.Fragment>
-										{getStepContent(activeStep)}
-										<div className={classes.buttons}>
-											{activeStep !== 0 && (
-												<Button
-													onClick={this.handleBack}
-													className={classes.button}
-												>
-													Back
-												</Button>
-											)}
-											<Button
-												variant="contained"
-												color="primary"
-												onClick={this.handleNext}
-												className={classes.button}
-											>
-												{activeStep === steps.length - 1
-													? "Place order"
-													: "Next"}
-											</Button>
-										</div>
-									</React.Fragment>
-								)}
-							</React.Fragment>
-						</CardBody>
-					</Card>
-				</div>
-			</NavBarLayout>
-		);
-	}
-}
-
-const styles = theme => ({
-	layout: {
-		width: "auto",
-		marginLeft: theme.spacing.unit * 2,
-		marginRight: theme.spacing.unit * 2,
-		[theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-			width: 600,
-			marginLeft: "auto",
-			marginRight: "auto"
-		}
-	},
-	paper: {
-		padding: theme.spacing.unit * 2,
-		[theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-			padding: theme.spacing.unit * 3
-		}
-	},
-	stepper: {
-		padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`
-	},
-	buttons: {
-		display: "flex",
-		justifyContent: "flex-end"
-	},
-	button: {
-		marginTop: theme.spacing.unit * 3,
-		marginLeft: theme.spacing.unit
-	}
-});
-
-Checkout.propTypes = {
-	classes: PropTypes.object.isRequired
-};
-
-const mapStateToProps = (state) => {
-    return {
-    	user: state.user,
-        cart: state.cart,
-		message: state.messages
+    switch (step) {
+        case 0:
+            return <Shipping />;
+        case 1:
+            return <Billing />;
+        case 2:
+            return <Items />;
+        case 3:
+            return <Review />;
+        default:
+            throw new Error("Unknown step");
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators(orderActions, dispatch);
+class Checkout extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(Checkout));
+    componentWillMount() {
+        this.props.prepareOrder();
+    }
+
+    state = {
+        activeStep: 0
+    };
+
+    handleNext = () => {
+        const { activeStep } = this.state;
+        this.setState({
+            activeStep: activeStep + 1
+        });
+    };
+
+    handleBack = () => {
+        const { activeStep } = this.state;
+        this.setState({
+            activeStep: activeStep - 1
+        });
+    };
+
+    handleReset = () => {
+        this.setState({
+            activeStep: 0
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { activeStep } = this.state;
+
+        return (
+            <NavBarLayout>
+                {this.props.message.messages.map((message, i) => (
+                    <Alert message={message} index={i} />
+                ))}
+
+                <div className={classes.container}>
+                    <div className={classes.title}>
+                        <Typography variant="h4" color="secondary">
+                            CHECKOUT
+                        </Typography>
+                    </div>
+                    <Stepper
+                        activeStep={activeStep}
+                        className={classes.stepper}
+                    >
+                        {steps.map(label => (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    <React.Fragment>
+                        {activeStep === steps.length ? (
+                            <React.Fragment>
+                                <Typography variant="headline" gutterBottom>
+                                    Thank you for your order.
+                                </Typography>
+                                <Typography variant="subheading">
+                                    Your order number is #2001539. We have
+                                    emailed your oder confirmation, and will
+                                    send you an update when your order has
+                                    shipped.
+                                </Typography>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                {getStepContent(activeStep)}
+                                <div className={classes.buttons}>
+                                    {activeStep !== 0 && (
+                                        <Button
+                                            onClick={this.handleBack}
+                                            className={classes.button}
+                                        >
+                                            Back
+                                        </Button>
+                                    )}
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={this.handleNext}
+                                        className={classes.button}
+                                    >
+                                        {activeStep === steps.length - 1
+                                            ? "Place order"
+                                            : "Next"}
+                                    </Button>
+                                </div>
+                            </React.Fragment>
+                        )}
+                    </React.Fragment>
+                </div>
+            </NavBarLayout>
+        );
+    }
+}
+
+const styles = theme => ({
+    container: {
+        border: "solid 1px",
+        borderColor: "#CCCCCC",
+        padding: theme.spacing.unit * 4,
+        [theme.breakpoints.up("md")]: {
+            marginLeft: 50,
+            marginRight: 50
+        },
+        [theme.breakpoints.up("lg")]: {
+            marginLeft: 150,
+            marginRight: 150
+        },
+        [theme.breakpoints.up("xl")]: {
+            marginLeft: 250,
+            marginRight: 250
+        }
+    },
+    title: {
+        backgroundColor: "#FF9933",
+        padding: 5,
+        marginBottom: theme.spacing.unit * 4,
+        textAlign: "center",
+        marginLeft: theme.spacing.unit * -4,
+        marginRight: theme.spacing.unit * -4
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+            padding: theme.spacing.unit * 3
+        }
+    },
+    stepper: {
+        padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`,
+		backgroundColor: "#fafafa",
+    },
+    buttons: {
+        display: "flex",
+        justifyContent: "flex-end"
+    },
+    button: {
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: theme.spacing.unit
+    }
+});
+
+Checkout.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+        cart: state.cart,
+        message: state.messages
+    };
+};
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(orderActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles, { withTheme: true })(Checkout));
