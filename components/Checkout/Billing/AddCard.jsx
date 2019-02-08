a9409dbad4b31842832d0ecba7c6de1f5a83fdb0
Dimitri Vasilev <dimitrovasilev@gmail.com>
2019-02-07 22:27:41 -0800

    Checkout folder added back to components

---
+++ components/Checkout/Billing/AddCard.jsx
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import Utils from "../../../lib/Utils";
+
+// custom
+import SalesLib from "../../../lib/SalesLib";
+import { userActions } from "../../../redux/actions/userActions";
+
+class AddCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            card: {
+                name: "",
+                number: "",
+                expireMonth: this.currentMonth,
+                expireYear: this.currentYear
+            },
+            expirationDates: Utils.getExpirationDates()
+        };
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <React.Fragment>
+                <Grid container spacing={24}>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            required
+                            id="cardName"
+                            label="Name on card"
+                            fullWidth
+                            value={this.state.card.name}
+                            onChange={event =>
+                                this.setState({
+                                    card: {
+                                        ...this.state.card,
+                                        name: event.target.value
+                                    }
+                                })
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            required
+                            id="cardNumber"
+                            label="Card number"
+                            fullWidth
+                            value={this.state.card.number}
+                            onChange={event =>
+                                this.setState({
+                                    card: {
+                                        ...this.state.card,
+                                        number: event.target.value
+                                    }
+                                })
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            select
+                            required
+                            id="expireMonth"
+                            name="expireMonth"
+                            label="Month"
+                            fullWidth
+                            autoComplete={this.currentMonth}
+                            value={this.state.card.expireMonth}
+                            onChange={event =>
+                                this.setState({
+                                    card: {
+                                        ...this.state.card,
+                                        expireMonth: event.target.value
+                                    }
+                                })
+                            }
+                        >
+                            <MenuItem value="0">1</MenuItem>
+                            <MenuItem value="1">2</MenuItem>
+                            <MenuItem value="2">3</MenuItem>
+                            <MenuItem value="3">4</MenuItem>
+                            <MenuItem value="4">5</MenuItem>
+                            <MenuItem value="5">6</MenuItem>
+                            <MenuItem value="6">7</MenuItem>
+                            <MenuItem value="7">8</MenuItem>
+                            <MenuItem value="8">9</MenuItem>
+                            <MenuItem value="9">10</MenuItem>
+                            <MenuItem value="10">11</MenuItem>
+                            <MenuItem value="11">12</MenuItem>
+                        </TextField>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            select
+                            required
+                            id="expireYear"
+                            name="expireYear"
+                            label="Year"
+                            fullWidth
+                            autoComplete={this.currentYear}
+                            value={this.state.card.expireYear}
+                            onChange={event =>
+                                this.setState({
+                                    card: {
+                                        ...this.state.card,
+                                        expireYear: event.target.value
+                                    }
+                                })
+                            }
+                        >
+                            {this.state.expirationDates.map((date, i) => {
+                                return (
+                                    <MenuItem key={i} value={date.year}>
+                                        {date.year}
+                                    </MenuItem>
+                                );
+                            })}
+                        </TextField>
+                    </Grid>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+}
+
+const styles = theme => ({
+    addressBox: {
+        border: "solid 1px",
+        borderColor: "#CCCCCC",
+        padding: theme.spacing.unit * 2
+    }
+});
+
+AddCard.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+        order: state.order
+    };
+};
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(userActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles)(AddCard));
---
+++ components/Checkout/Items/Items.jsx
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ItemPanel from "./ItemPanel";
+import { orderActions } from "../../../redux/actions/orderActions";
+
+class Items extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <React.Fragment>
+                {
+                    this.props.order.isLoading ?
+                        <Grid
+                        container
+                        justify="center"
+                        alignItems="center"
+                        spacing={24}
+                        >
+                            <CircularProgress size={50} />
+                        </Grid>
+                    :
+                        <Grid
+                        container
+                        justify="center"
+                        alignItems="center"
+                        spacing={24}
+                        >
+                            <Grid item xs={12} md={9}>
+                                <Typography variant="h6" color="textPrimary">
+                                    ITEMS
+                                </Typography>
+                                <div className={classes.sectionTitleDivider} />
+                            </Grid>
+
+                            <Grid item xs={12} md={3} style={{textAlign: "center"}}>
+                                <TextField
+                                    select
+                                    required
+                                    id="shippingOption"
+                                    name="shippingOption"
+                                    label="Shipping Option"
+                                    variant="outlined"
+                                    autoComplete="country"
+                                    fullWidth
+                                    value={this.props.order.selectedShippingOption}
+                                    onChange={event =>
+                                        this.props.setShippingOption(event.target.value)
+                                    }
+                                    >
+                                    {this.props.order.shippingOptions.map(
+                                        (option, i) => (
+                                            <MenuItem key={i} value={option}>
+                                                {option}
+                                            </MenuItem>
+                                        )
+                                    )}
+                                </TextField>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <List>
+                                    {this.props.order.items.map((item, i) => (
+                                        <ItemPanel key={i} item={item} index={i} />
+                                    ))}
+
+                                    <ListItem className={classes.listItem}>
+                                        <ListItemText primary="Item Subtotal" />
+                                        <Typography
+                                            variant="subheading"
+                                            color="primary"
+                                            className={classes.total}
+                                            >
+                                            {this.props.order.itemSubtotal}
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem className={classes.listItem}>
+                                        <ListItemText primary="Shipping Cost" />
+                                        <Typography
+                                            variant="subheading"
+                                            color="primary"
+                                            className={classes.total}
+                                            >
+                                            {this.props.order.shippingSubtotal}
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem className={classes.listItem}>
+                                        <ListItemText primary="Order Subtotal" />
+                                        <Typography
+                                            variant="subheading"
+                                            color="primary"
+                                            className={classes.total}
+                                            >
+                                            {this.props.order.orderSubtotal}
+                                        </Typography>
+                                    </ListItem>
+                                </List>
+                            </Grid>
+                        </Grid>
+                }                
+            </React.Fragment>
+        );
+    }
+}
+
+const styles = theme => ({
+    sectionTitleDivider: {
+        borderTop: "solid 1.5px",
+        borderColor: "#CCCCCC",
+        marginBottom: 10
+    },
+    listItem: {
+        padding: `${theme.spacing.unit}px 0`,
+		paddingRight: 20,
+		paddingLeft: 20,
+    },
+    total: {
+        fontWeight: "700"
+    },
+    title: {
+        marginTop: theme.spacing.unit * 2
+    }
+});
+
+Items.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+    return {
+        order: state.order
+    };
+};
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(orderActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles)(Items));
