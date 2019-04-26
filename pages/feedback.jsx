e81893e6f569370ed325cd5e2c7f651903b4f5f8
reactexcel <arun.etech2011@gmail.com>
2019-04-26 10:43:56 +0530

    working on feedback screen

--- pages/feedback.jsx
+++ pages/feedback.jsx
@@ -2,16 +2,41 @@ import React, { Component } from 'react'
 import NavBarLayout from "components/NavBar/NavBarLayout";
 import PageContainer from 'components/UI/PageContainer';
 import Typography from '@material-ui/core/Typography';
+import StarRatings from "react-star-ratings";
 
 class Feedback extends Component {
+
+  // changeRating( newRating, name ) {
+  //   this.setState({
+  //     rating: newRating
+  //   });
+  // }
+  state={
+    rating:4
+  }
+
+
   render() {
     return (
         <NavBarLayout>
+          <div className='flex-center'>
            <PageContainer heading="Feedback" >
-           <Typography component="h2" variant="display4" gutterBottom>
+           <div className='flex-center'>
+           <Typography  component="h2" variant="Title" gutterBottom>
           How was your experience with the app?
         </Typography>
+        hjvjhvjhvjv
+        </div>
+        {/* <StarRatings
+          rating={this.state.rating}
+          starRatedColor="blue"
+          // changeRating={this.changeRating}
+          numberOfStars={6}
+          name='rating'
+        /> */}
+     
            </PageContainer>
+           </div>
         </NavBarLayout>
     )
   }
